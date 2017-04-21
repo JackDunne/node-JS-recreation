456d99a12386eedd0661c929c869978b04a1ad00
JackDunne <8jdunne@stpatrick.org>
2017-04-21 10:55:49 -0500

    got buttons to change opacity

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
-function buttons(){
+(function(){
   const doc = document;
   let buttons = doc.querySelectorAll("article div");
   for(let x = 0;x<buttons.length;x++){
-  buttons[x].addEventListener("mouseover",function({
-    button[x].style.opacity = "75%";
-    }));
+  buttons[x].addEventListener("mouseover",function(){
+    buttons[x].style.opacity = "0.75";
+  });
   }
-}
+
+  for(let x = 0;x<buttons.length;x++){
+  buttons[x].addEventListener("mouseout",function(){
+    buttons[x].style.opacity = "1";
+  });
+  }
+})();
